c03b8a8bc8bd9aa7ea236a777fd3320019223573
rmcampigotto <rmcampigotto@gmail.com>
2024-04-18 17:35:43 -0300

    cors-implementation-1804

--- avaliacao/back/src/main.ts
+++ avaliacao/back/src/main.ts
@@ -1,11 +1,11 @@
 import app from './app'
-import api from './api'
+import cors from 'cors'
 
 function main() {
     app.listen(3000, 'localhost', () => {
         console.log('Server running at port 3000')
     })
-
+    app.use(cors())
 }
 
 main()
